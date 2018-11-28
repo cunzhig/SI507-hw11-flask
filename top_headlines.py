c9880272c0ba967cbfc7433292828ee1eb15731d
cunzhig <42757911+cunzhig@users.noreply.github.com>
2018-11-28 11:36:52 -0500

    Add files via upload
    fix ec1 api_key

--- top_headlines.py
+++ top_headlines.py
@@ -13,7 +13,7 @@ def welcome():
 def get_stories(section):
     baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
     extendedurl = baseurl + section + '.json'
-    params={'api-key': nyt_key}
+    params={'api-key': api_key}
     return requests.get(extendedurl, params).json()
 
 def get_headlines(nyt_results_dict):
@@ -30,17 +30,44 @@ def top_headlines(nm):
     story_list_json = get_stories('technology')
     headlines = get_headlines(story_list_json)
     html = render_template('user.html',name=nm,section='technology',my_list=headlines)
-    html += "<a href = '/user/"+nm+"/home'> Go to home </a>"
+    html += "\n<a href = '/user/"+nm+"/home'>home </a>"
+    html += "\n<a href = '/user/"+nm+"/opinion'>opinion </a>"
+    html += "\n<a href = '/user/"+nm+"/world'>world </a>"
+    html += "\n<a href = '/user/"+nm+"/national'>national </a>"
+    html += "\n<a href = '/user/"+nm+"/politics'>politics </a>"
+    html += "\n<a href = '/user/"+nm+"/nyregion'>nyregion </a>"
+    html += "\n<a href = '/user/"+nm+"/business'>business </a>"
+    html += "\n<a href = '/user/"+nm+"/technology'>technology </a>"
+    html += "\n<a href = '/user/"+nm+"/science'>science </a>"
+    html += "\n<a href = '/user/"+nm+"/health'>health </a>"
+    html += "\n<a href = '/user/"+nm+"/sports'>sports </a>"
+    html += "\n<a href = '/user/"+nm+"/arts'>arts </a>"
+    html += "\n<a href = '/user/"+nm+"/books'>books </a>"
+    
+    html += "\n<a href = '/user/"+nm+"/movies'>movies </a>"
+    html += "\n<a href = '/user/"+nm+"/theater'>theater </a>"
+    html += "\n<a href = '/user/"+nm+"/sundayreview'>sundayreview </a>"
+    html += "\n<a href = '/user/"+nm+"/fashion'>fashion </a>"
+    html += "\n<a href = '/user/"+nm+"/tmagazine'>tmagazine </a>"
+    html += "\n<a href = '/user/"+nm+"/food'>food </a>"
+    html += "\n<a href = '/user/"+nm+"/travel'>travel </a>"
+    html += "\n<a href = '/user/"+nm+"/magazine'>magazine </a>"
+    html += "\n<a href = '/user/"+nm+"/realestate'>realestate </a>"
+    html += "\n<a href = '/user/"+nm+"/automobiles'>automobiles</a>"
+    html += "\n<a href = '/user/"+nm+"/obituaries'>obituaries </a>"
+    html += "\n<a href = '/user/"+nm+"/insider'>insider</a>"
     return html
 
-
-@app.route('/user/<nm>/home')
-def ec1(nm):
-    story_list_json = get_stories('home')
+# user/<name>/home
+@app.route('/user/<nm>/<sec>')
+def ec1(nm,sec):
+    story_list_json = get_stories(sec)
     headlines = get_headlines(story_list_json)
-    html = render_template('user.html',name=nm,section='home',my_list=headlines)
+    html = render_template('user.html',name=nm,section=sec,my_list=headlines)
     html += "<a href = /user/"+nm+"> Go Back </a>"
     return html
+
+
 
 
 if __name__ == '__main__':
